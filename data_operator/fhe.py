7612a4bfa3192e74da5bac77439ff00dc57b9eda
Ubuntu <ubuntu@ip-172-31-46-131.ec2.internal>
2024-06-16 14:40:13 +0000

    fix: change key path

--- data_operator/fhe.py
+++ data_operator/fhe.py
@@ -2,6 +2,7 @@ import pandas as pd
 from concrete.ml.pandas import ClientEngine
 import os
 import asyncio
+from io import StringIO
 
 client = ClientEngine(keys_path="./key/2pm-key")
 
@@ -15,11 +16,11 @@ def validate_file_type(file_name: str):
 
 async def convert_file_to_df(file_name: str, contents: bytes):
     if file_name.split(".")[-1] == "csv":
-        return pd.read_csv(contents)
+        return pd.read_csv(StringIO(contents))
     elif file_name.split(".")[-1] == "xlsx" or file_name.split(".")[-1] == "xls":
-        return pd.read_excel(contents)
+        return pd.read_excel(StringIO(contents))
     elif file_name.split(".")[-1] == "json":
-        return pd.read_json(contents)
+        return pd.read_json(StringIO(contents))
     else:
         raise ValueError("Invalid file type")
 
@@ -27,8 +28,10 @@ async def convert_file_to_df(file_name: str, contents: bytes):
 async def fhe_encrypt(file_path: str, file_name: str):
     with open(f"{file_path}/{file_name}", "r") as f:
         data = f.read()
-    df = convert_file_to_df(file_name, data)
+    df = await convert_file_to_df(file_name, data)
     df_entrypted = client.encrypt_from_pandas(df)
+    if not os.path.exists(ENCRYPTED_DIRECTORY):
+        os.makedirs(ENCRYPTED_DIRECTORY)
     df_entrypted.save(f"{ENCRYPTED_DIRECTORY}/encrypted-{file_name}")
     return f"{ENCRYPTED_DIRECTORY}/encrypted-{file_name}"
 
